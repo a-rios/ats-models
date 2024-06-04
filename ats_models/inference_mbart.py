aa858928c32db3f18a46a9aa2d592c15338a7fb6
Lukas <fischerl@cl.uzh.ch>
2024-06-04 14:53:00 +0200

    debug inference

--- ats_models/inference_mbart.py
+++ ats_models/inference_mbart.py
@@ -147,7 +147,6 @@ class Inference(pl.LightningModule):
         logging.info(f"generation_config: {generation_config}")
 
         if decoder_start_token_ids is not None: # no reference but list of target language tags given
-            decoder_start_token_ids = decoder_start_token_ids.unsqueeze(1)
             logging.info("elif decoder_start_token_ids is not None: # no reference but list of target language tags given")
             logging.info(f"generating ids with input_ids: {input_ids.shape}, attention_mask: {attention_mask.shape}, generation_config: {generation_config}, decoder_start_token_ids: {decoder_start_token_ids}")
             generated_ids = self.model.generate(input_ids=input_ids,
